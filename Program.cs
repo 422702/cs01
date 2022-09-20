49561e4d9580a83b47a6a4030e04fb46a07baa0d
Andrey <422702@inbox.ru>
2022-09-21 00:28:12 +0700

    Дописал

--- Program.cs
+++ Program.cs
@@ -21,3 +21,9 @@ int number3= int.Parse(input);
 if(number1>number2&&number1>number3)Console.WriteLine($"Чило{number1} больше");
 if(number2>number1&&number2>number3)Console.WriteLine($"Чило{number2} больше");
 if(number3>number2&&number3>number1)Console.WriteLine($"Чило{number3} больше");
+
+Console.WriteLine("Задача№6");
+Console.WriteLine("Введите число");
+input=Console.ReadLine(  );
+number1= int.Parse(input);
+if((number1 % 2)==0)Console.WriteLine("число чётное"); else Console.WriteLine("число не чётное");
