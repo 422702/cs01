babe0bc85c7b0a77d3e90359bc1b54ed4a38f75e
Andrey <422702@inbox.ru>
2022-09-20 23:48:40 +0700

    Второй

--- Program.cs
+++ Program.cs
@@ -1,2 +1,9 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+Console.WriteLine("Задача№2");
+Console.WriteLine("Введите число");
+string input=Console.ReadLine(  );
+int number1= int.Parse(input);
+Console.WriteLine("Введите число");
+input=Console.ReadLine();
+int number2= int.Parse(input);
+if(number1>number2)Console.WriteLine($"Чило{number1} больше"); else Console.WriteLine($"Чило{number2} больше");
