e44ee788146912d2e233dbfc553657d53dc76ba4
Andrey <422702@inbox.ru>
2022-09-20 23:59:26 +0700

    Второй

--- Program.cs
+++ Program.cs
@@ -7,3 +7,16 @@ Console.WriteLine("Введите число");
 input=Console.ReadLine();
 int number2= int.Parse(input);
 if(number1>number2)Console.WriteLine($"Чило{number1} больше"); else Console.WriteLine($"Чило{number2} больше");
+
+Console.WriteLine("Задача№4");
+Console.WriteLine("Введите число");
+string input=Console.ReadLine(  );
+int number1= int.Parse(input);
+Console.WriteLine("Введите число");
+input=Console.ReadLine();
+int number2= int.Parse(input);
+input=Console.ReadLine();
+int number3= int.Parse(input);
+if(number1>number2&&number1>number3)Console.WriteLine($"Чило{number1} больше");
+if(number2>number1&&number2>number3)Console.WriteLine($"Чило{number2} больше");
+if(number3>number2&&number3>number1)Console.WriteLine($"Чило{number3} больше");
